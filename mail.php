df5925a9d52ef50d64a5e817ba8145b06a29866e
harshitsidhwa <harshit.sidhwa@gmail.com>
2018-08-09 02:31:29 +0530

    Removing Error

--- mail.php
+++ mail.php
@@ -2,7 +2,7 @@
 	require 'PHPMailer/PHPMailerAutoload.php';
 	// enter your credentials
 	$email = "someone@example.com";
-	$password = "password";
+	$password = "somepassword";
 	// initialize the object
 	$mail = new PHPMailer;
 	$mail->isSMTP();
@@ -29,25 +29,25 @@
     	$line=trim($line);
     	$line = explode(',', $line);
     	// get the data from the csv
-		$toEmailId = $line[1];
-		$toName = $line[0];
+	$toEmailId = $line[1];
+	$toName = $line[0];
 		
-		// sample template.html to be send
-		$message = file_get_contents('template_7-8-2018.html');
-		$message = trim($message);
-		// add custom details to the mail
-		$message = str_replace('<!--name-->', $toName, $message);
+	// sample template.html to be send
+	$message = file_get_contents($mailTemplate);
+	$message = trim($message);
+	// add custom details to the mail
+	$message = str_replace('<!--name-->', $toName, $message);
 
-		$mail->clearAllRecipients();
-		$mail->addAddress($toEmailId, $toName);
-		$mail->Subject = $subject;
-		$mail->msgHTML($message);
-		// test whther the mail is send succesfully or not
-		if (!$mail->send()) {
-			$error = $toEmailId."\nMailer Error: " . $mail->ErrorInfo."\n\n";
-			echo  $error;
-		} else {
-			echo "To ".$toName."\nEmail: ".$toEmailId."\nRequest sent Successfully\n\n";
-		}
+	$mail->clearAllRecipients();
+	$mail->addAddress($toEmailId, $toName);
+	$mail->Subject = $subject;
+	$mail->msgHTML($message);
+	// test whther the mail is send succesfully or not
+	if (!$mail->send()) {
+		$error = $toEmailId."\nMailer Error: " . $mail->ErrorInfo."\n\n";
+		echo  $error;
+	} else {
+		echo "To ".$toName."\nEmail: ".$toEmailId."\nRequest sent Successfully\n\n";
 	}
+   }
 ?>
